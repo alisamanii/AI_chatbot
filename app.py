7a7071be32e806c18673ccd7d54e66125985295e
Ali Samani <113721774+alisamanii@users.noreply.github.com>
2025-04-15 14:08:06 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import telebot
+import google.generativeai as genai
+from translate import Translator
+translator=Translator(to_lang="es")
+
+bot=telebot.TeleBot(BOT_TOKEN)
+
+Api="your_API_Gemeni" 
+model_name="gemini-2.0-flash"
+genai.configure(api_key=Api)
+model=genai.GenerativeModel(model_name)
+
+@bot.message_handler(func=lambda message:True)
+def chat(message):
+    user_input=message.text
+    response=model.generate_content(user_input)
+    translation=translator.translate(response.text)
+    bot.reply_to(message,translation)
+
+
+bot.infinity_polling()
